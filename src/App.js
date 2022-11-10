68dab952405492673ab428b240f5818773f3fff1
Muhammad Anas Raza Siddiqui <anasattari48@gmail.com>
2022-11-10 22:49:06 +0500

    initial commit
    this  repo will help in making react reusable component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import StickyFooter from './Component/Footer';
+import Nav from './Component/Nav';
+function App() {
+  return (
+    <div>
+      <Nav/>
+      <StickyFooter/>
+    </div>
+  );
+}
+
+export default App;
